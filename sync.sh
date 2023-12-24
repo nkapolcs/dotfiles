9d301ed675eef2f1da2b507fd664b200f2f095ce
nkapolcs <nagy.kapolcs@gmail.com>
2023-12-25 00:47:02 +0100

    update dotfiles

---
+++ sync.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+basename="$HOME/projects/dotfiles"
+backup=~/.dotfiles_backup/$(date +%m%d%H%M%Y%S)
+
+for file in $(find $basename -maxdepth 1 -iname '.*' -not -path '*/.git')
+do
+  filename=$(basename $file)
+  mv "$HOME/$filename" "$backup" 2>/dev/null
+  rm -rf "$HOME/$filename"
+  echo " $file => ~/$filename"
+  ln -s "$file" ~
+done
